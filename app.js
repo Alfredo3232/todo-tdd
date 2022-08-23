666fd321dbe4bc94bac3e57d3faefdd89b17f422
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-08-23 12:32:56 -0700

    fixing some issues

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express')
 const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.json('Hello World!');
 });
 
 app.listen(3000, () => {
