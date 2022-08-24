55ff91288408556b2ffc5be7f4ac13f0e6137646
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-08-24 15:10:35 -0700

    more error handling

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,5 +1,6 @@
 const TodoModel = require("../model/todo.model");
 
+
 exports.createTodo = async (req, res, next) => {
     try {
         const createdModel = await TodoModel.create(req.body);
@@ -8,3 +9,12 @@ exports.createTodo = async (req, res, next) => {
         next(error);
     }
 };
+
+exports.getTodos = async (req, res, next) => {
+    try {
+        const todos = await TodoModel.find({});
+        res.status(200).json(todos);
+    } catch (error) {
+        next(error);
+    }
+}
