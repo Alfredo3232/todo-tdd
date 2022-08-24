ac628bba176bad59330eeb882f87f4d8647ed10a
arojas32 <Alfredo2001@me.com>
2022-08-23 20:01:09 -0700

    made progress and fixed huge error in code

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require("express")
+const express = require("express");
+const todoRoutes = require("./routes/todo.routes");
 const app = express();
+const mongodb = require("./mongodb/mongodb.connect");
 
+mongodb.connect()
+    // .then((err) => {
+    //     console.log('mongo connect if its null => ', err)
+    // })
+
+app.use(express.json());
+
+app.use("/todos", todoRoutes)
 
 app.get('/', (req, res) => {
     res.json('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log('Server is Running!');
-});
+// app.listen(3000, () => {
+//     console.log('Server is Running!');
+// });
 
 module.exports = app;
