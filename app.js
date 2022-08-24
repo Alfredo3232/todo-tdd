ef805610fa0e287042e8d52e20a6f5868a5bfc38
arojas32 <Alfredo2001@me.com>
2022-08-24 15:10:35 -0700

    more error handling

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const todoRoutes = require("./routes/todo.routes");
 const app = express();
 const mongodb = require("./mongodb/mongodb.connect");
 
-mongodb.connect()
+mongodb.connect();
 
 app.use(express.json());
 
-app.use("/todos", todoRoutes)
+app.use("/todos", todoRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+});
 
 app.get('/', (req, res) => {
     res.json('Hello World!');
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -5,19 +5,35 @@ const httpMocks = require("node-mocks-http");
 const newTodo = require('../mock-data/new-todo.json');
 
 TodoModel.create = jest.fn();
+TodoModel.find = jest.fn();
 
 let req, res, next;
 beforeEach(() => {
     req = httpMocks.createRequest();
     res = httpMocks.createResponse();
     next = jest.fn();
-})
+});
+
+describe("TodoController.getTodos", () => {
+    it("should have a getTodos function", () => {
+        expect(typeof TodoController.getTodos).toBe("function");
+    });
+    it("should call TodoModel.find({})", async () => {
+        await TodoController.getTodos(req, res, next);
+        expect(TodoModel.find).toHaveBeenCalledWith({});
+    });
+    it("should return response with status 200 and all todos", async () => {
+        await TodoController.getTodos(req, res, next);
+        expect(res.statusCode).toBe(200);
+        expect(res._isEndCalled()).toBeTruthy();
+    })
+});
 
 describe("TodoController.createTodo", () => {
 
     beforeEach(() => {
         req.body = newTodo;
-    })
+    });
 
     it("should have a createTodo function", () => {
         expect(typeof TodoController.createTodo).toBe("function");
