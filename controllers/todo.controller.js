665b6567431280cfe0e871cd3a5455c43b58ed70
arojas32 <Alfredo2001@me.com>
2022-08-24 17:38:59 -0700

    progress

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -12,9 +12,19 @@ exports.createTodo = async (req, res, next) => {
 
 exports.getTodos = async (req, res, next) => {
     try {
-        const todos = await TodoModel.find({});
-        res.status(200).json(todos);
+        const allTodos = await TodoModel.find({});
+        res.status(200).json(allTodos);
     } catch (error) {
         next(error);
     }
-}
+};
+
+exports.getTodoById = async (req, res, next) => {
+    try {
+        const todoIds = await TodoModel.findById(req.params.todoId);
+        res.status(200).json(todoIds);
+    } catch (error) {
+        next(error);
+    }
+
+};
